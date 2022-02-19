431ac2460f309e2e15d964af35e02e6bd983710f
Naveen Sharma <vatssharma474@gmail.com>
2022-02-19 11:41:11 +0530

    update Node

--- src/ErrorPage.js
+++ src/ErrorPage.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function ErrorPage() {
-    return <div><h1>page Not Found</h1></div>;
+    const Navigate = useNavigate();
+    return <div className='error-page'>
+        <h1>404</h1>
+        <h3>page Not Found</h3>
+        <button onClick={() => Navigate("/")} type="button" className="btn btn-primary">Go Back</button>
+    </div >;
 }
